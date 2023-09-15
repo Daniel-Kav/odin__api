ef05ea965ca9a229fcbfd261b2d918186fe667a2
daniel-kav <dankavatha01@gmail.com>
2023-09-15 17:28:46 +0300

    trying to fetch images

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const img = document.querySelector('img');
-
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=XRVczzOZqT3VCrsH8ZL2ncdQabfMRNT7&s=cats', {mode: 'cors'})
-    .then(function (response){
-        return response.json();
-    })
-    .then(function(response) {
-      console.log(response.json());
-});
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    img.src = response.data.images.original.url;
+  });
