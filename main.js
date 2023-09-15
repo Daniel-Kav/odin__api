7a0359fc40f32d1a31566810fcdd791199085dde
daniel-kav <dankavatha01@gmail.com>
2023-09-15 09:03:18 +0300

    added the api i want to fetch from

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const img = document.querySelector('img');
 
-const img = document.querySelector('img');
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=XRVczzOZqT3VCrsH8ZL2ncdQabfMRNT7&s=cats', {mode: 'cors'})
+    .then(function (response){
+        return response.json();
+    })
     .then(function(response) {
       console.log(response.json());
 });
