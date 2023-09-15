0cea33c6022299f48516fe928de328cc4e08412a
daniel-kav <dankavatha01@gmail.com>
2023-09-15 20:40:03 +0300

    adding functinality to the input

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const img = document.querySelector('img');
+const searchInput = document.querySelector('#search');
+const searchBtn = document.querySelector('#searchBtn');
+const newImageBtn = document.querySelector('#newImageBtn');
+const apiKey = 'XRVczzOZqT3VCrsH8ZL2ncdQabfMRNT7'
 //fetching the image from giffy
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=XRVczzOZqT3VCrsH8ZL2ncdQabfMRNT7&s=cats', {mode: 'cors'})
   .then(function(response) {
