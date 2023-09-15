860bd948545438001ca50586cda9a14a0e27a524
daniel-kav <dankavatha01@gmail.com>
2023-09-15 17:31:58 +0300

    successfully feched images from giffy api

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const img = document.querySelector('img');
+//fetching the image from giffy
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=XRVczzOZqT3VCrsH8ZL2ncdQabfMRNT7&s=cats', {mode: 'cors'})
   .then(function(response) {
     return response.json();
