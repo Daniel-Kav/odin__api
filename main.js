50ba2e2ba6748d484f90b3b0d57f7e5071aa58a7
daniel-kav <dankavatha01@gmail.com>
2023-09-15 20:51:31 +0300

    fetching random image function

--- main.js
+++ main.js
@@ -11,3 +11,25 @@ fetch('https://api.giphy.com/v1/gifs/translate?api_key=XRVczzOZqT3VCrsH8ZL2ncdQa
   .then(function(response) {
     img.src = response.data.images.original.url;
   });
+
+//function to fetch  a random GIF based on the search term
+function fetchRandomGif(searchTerm) {
+  const apiUrl = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${searchTerm}`;
+
+  fetch(apiUrl,{mode: 'cors'})
+    .then(function (response) {
+      if(!response.ok){
+        throw new Error('Giphy API request Failed');
+      }
+      return response.json();
+    })
+    .then(function (response) {
+      if (response.data && response.data.images && response.data.images.original) {
+        img.src = response.data.images.original.url;
+      }else {
+        //display a default image or an error message
+        img.src = 'default-image.png';
+      }
+    });
+}
+fetchRandomGif('cats');
